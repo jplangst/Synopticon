49d037bb9b26a969783489671f24279b5af2fd13
Jens-Patrick <jens.patrick.langstrand@ife.no>
2019-06-26 14:30:37 +0200

    Update CleanWampHeader.h
    Cleaned the file

--- Synopticon/Source/Synopticon/Utils/CleanWampHeader.h
+++ Synopticon/Source/Synopticon/Utils/CleanWampHeader.h
@@ -16,36 +16,15 @@ THIRD_PARTY_INCLUDES_START
 #include "AllowWindowsPlatformAtomics.h"
 #endif
 
-
-//#include "ThirdParty/OpenSSL/1.0.2g/include/Win64/VS2015/openssl/pem.h"
-//#include "openssl/ssl.h"
-//#include "ThirdParty/OpenSSL/1.0.2g/include/Win64/VS2015/openssl/rsa.h"
-//#include "ThirdParty/OpenSSL/1.0.2g/include/Win64/VS2015/openssl/evp.h"
-//#include "ThirdParty/OpenSSL/1.0.2g/include/Win64/VS2015/openssl/bio.h"
-//#include "ThirdParty/OpenSSL/1.0.2g/include/Win64/VS2015/openssl/err.h"
-//#include "ThirdParty/OpenSSL/1.0.2g/include/Win64/VS2015/openssl/bn.h"
-//#include "ThirdParty/OpenSSL/1.0.2g/include/Win64/VS2015/openssl/x509.h"
-//#include "openssl/hmac.h"
-
-
-//#include "MacroUtility/UndefineMacros_UE_4.20.h"
 #include <autobahn.hpp>
 #include <wamp_websocketpp_websocket_transport.hpp>
 
-
-//#include "MacroUtility/RedefineMacros_UE_4.20.h"
 #if PLATFORM_WINDOWS
 #include "HideWindowsPlatformAtomics.h"
 #include "HideWindowsPlatformTypes.h"
 #endif
 
 using namespace autobahn;
-//#include "AllowWindowsPlatformTypes.h"
-//#include "AllowWindowsPlatformAtomics.h"
-//#include <wamp_websocketpp_websocket_transport.hpp>
-//#include <autobahn.hpp>
-//#include "HideWindowsPlatformAtomics.h"
-//#include "HideWindowsPlatformTypes.h"
 
 THIRD_PARTY_INCLUDES_END
 #undef UI
